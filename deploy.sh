51ca979ef3c49c3e168555b90b74d3bf8c0334f6
msaim04_cyber <me.msaim04@gmail.com>
2025-10-07 07:52:00 -0700

    dsddd

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-cd ~/app
+cd ~/app  
 sudo docker build -t testing:latest .
 sudo docker rm -f testing || true
 sudo docker run -d --name testing --restart unless-stopped -p 80:3000 testing:latest
